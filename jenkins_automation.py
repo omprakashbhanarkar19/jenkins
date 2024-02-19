12810cbd5b6f9cd6e9cbb5120235f46e20102e44
omprakashbhanarkar <omprakashbhanarkar19@gmail.com>
2024-02-19 19:50:06 +0530

    updated

--- jenkins_automation.py
+++ jenkins_automation.py
@@ -4,9 +4,9 @@ import os
 
 
 
-host = "http://localhost:8080/"
-username = "Omprakash Bhanarkar" #jenkins username here
-password = "11a2ba3eb5e005c150e782f33b44f1e923" # Jenkins user password / api token here
+host = "http://54.87.147.137:8080/"
+username = "admin" #jenkins username here
+password = "11390f232fcee633ff7fc863ec99e41da1" # Jenkins user password / api token here
 server = jenkins.Jenkins(host, username=username, password=password) #automation_user_password
 
 user = server.get_whoami()
