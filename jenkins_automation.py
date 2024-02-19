f9060583f74af29060a122a65b74fc643f8f5a32
omprakashbhanarkar19 <85619205+omprakashbhanarkar19@users.noreply.github.com>
2024-02-19 18:43:52 +0530

    Update jenkins_automation.py

--- jenkins_automation.py
+++ jenkins_automation.py
@@ -17,7 +17,7 @@ print('Hello %s from Jenkins %s' % (user['fullName'], version))
 
 # #Create deployment jobs
 # #create a blank job
-# server.create_job("job1", jenkins.EMPTY_CONFIG_XML)
+ server.create_job("job1", jenkins.EMPTY_CONFIG_XML)
 # #create pre-configured-job
 # job2_xml = open("job2.xml", mode='r', encoding='utf-8').read()
 # server.create_job("job2", job2_xml)
